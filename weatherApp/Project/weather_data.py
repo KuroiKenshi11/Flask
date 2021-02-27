2b3dd49bcc51c268803794ee2b64f18a7a2c62b7
Mohammed Alfathi <iizekalfthi@gmail.com>
2021-02-27 23:42:33 +0400

    Modifying weather python file

--- weatherApp/Project/weather_data.py
+++ weatherApp/Project/weather_data.py
@@ -1,8 +1,6 @@
 import urllib.request
 import json
 
-# My Api Key: d787b14cd78be94197b35c82f2a06419
-
 
 def get_weatherData(city, api_key):
     url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
