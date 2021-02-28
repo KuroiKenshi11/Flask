6468f3dc991b673325970c573fc1fafec914ac43
Mohammed Alfathi <iizekalfthi@gmail.com>
2021-02-28 17:40:14 +0400

    Modifying python main file and weather file and add new files

--- weatherApp/Project/weather_data.py
+++ weatherApp/Project/weather_data.py
@@ -5,5 +5,25 @@ import json
 def get_weatherData(city, api_key):
     url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
     response = urllib.request.urlopen(url)
-    data = json.loads(response.read())
-    return data
+    weather = json.loads(response.read())
+    weather_info = {
+        "city_name": weather["name"],
+        "temp": str("{:.1f}").format((int(weather["main"]["temp"]) - 273)),
+        "coord_lon": weather["coord"]["lon"],
+        "coord_lat": weather["coord"]["lat"],
+        "id": weather["id"],
+        "main_weather": weather["weather"][0]["main"],
+        "description": weather["weather"][0]["description"],
+        "temp_feels_like": weather["main"]["feels_like"],
+        "temp_min": str("{:.1f}").format((int(weather["main"]["temp_min"]) - 273)),
+        "temp_max": str("{:.1f}").format((int(weather["main"]["temp_max"]) - 273)),
+        "pressure": weather["main"]["pressure"],
+        "humidity": weather["main"]["humidity"],
+        "visibility": weather["visibility"],
+        "wind_speed": weather["wind"]["speed"],
+        "wind_deg": weather["wind"]["deg"],
+        "clouds": weather["clouds"]["all"],
+        "timezone": weather["timezone"],
+        "country_code": weather["sys"]["country"],
+    }
+    return weather_info
--- weatherApp/run.py
+++ weatherApp/run.py
@@ -7,6 +7,8 @@ from Project import app, db
 from flask import render_template, redirect, url_for, request, flash
 from flask_login import login_user, logout_user, login_required, current_user
 from urllib.error import HTTPError
+from http.client import InvalidURL
+from werkzeug.exceptions import BadRequest
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -14,14 +16,21 @@ def index():
     if current_user.is_authenticated:
         try:
             form = TempretureSearch()
-            search = form.search.data
+            search = str(form.search.data).strip()
             weather = weather_data.get_weatherData(
-                city=search, api_key="d787b14cd78be94197b35c82f2a06419")
-            cityName = weather["name"]
-            temp = str("{:.1f}").format((int(weather["main"]["temp"]) - 273))
-            return render_template("index.html", form=form, city=cityName, temp=temp)
+                search, api_key="d787b14cd78be94197b35c82f2a06419")
+            name = weather["city_name"]
+            temp = weather["temp"]
+            min_temp = weather["temp_min"]
+            max_temp = weather["temp_max"]
+
+            return render_template("index.html", form=form,
+                                   name=name, temp=temp, min_temp=min_temp,
+                                   max_temp=max_temp)
         except HTTPError:
             return render_template("errors/HTTPError.html")
+        except InvalidURL:
+            return render_template("errors/HTTPInvalidurl.html")
     else:
         flash("First, You should Log in and then you can start!")
         return redirect(url_for("login"))
@@ -40,7 +49,7 @@ def register():
         flash('Thanks for registration!')
         return redirect(url_for('login'))
 
-    return render_template('register.html', form=form, error=error)
+    return render_template('register.html', form=form)
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -62,6 +71,21 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for("index"))
+
+
+@app.errorhandler(404)
+def handle_error_404(e):
+    return render_template("errors/404.html"), 404
+
+
+@app.errorhandler(405)
+def handle_error_405(e):
+    return render_template("errors/405.html"), 405
+
+
+@app.errorhandler(BadRequest)
+def handle_error_400(e):
+    return render_template("errors/400.html"), 400
 
 
 if __name__ == "__main__":
